86d8e9d25c243dd6f1525309108d3745e58512cd
Andreas Scheibel <contact@camya.com>
2022-11-05 11:03:46 +0100

    Removed commented

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -27,10 +27,5 @@ class TestCase extends Orchestra
     public function getEnvironmentSetUp($app)
     {
         config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_filament-seo_table.php.stub';
-        $migration->up();
-        */
     }
 }
