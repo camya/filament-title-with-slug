cf06952a426dee9035396fb7ae44a3f0ad9177b9
Andreas Scheibel <contact@camya.com>
2022-11-06 08:39:16 +0100

    Updated translation

--- resources/lang/en/package.php
+++ resources/lang/en/package.php
@@ -2,10 +2,10 @@
 
 return [
     'permalink_label' => 'Permalink:',
+    'permalink_label_link_visit' => 'Visit',
     'permalink_action_edit' => 'Edit',
     'permalink_action_ok' => 'OK',
     'permalink_action_reset' => 'Reset',
     'permalink_action_cancel' => 'Cancel',
-    'permalink_label_link_visit' => 'Visit',
     'permalink_status_changed' => 'changed',
 ];
