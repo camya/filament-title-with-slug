eebf6a8f15bcf34d100f95e412ec8afc8b289cea
Andreas Scheibel <contact@camya.com>
2022-11-16 17:45:33 +0100

    Replaced SVG with heroicon components

--- resources/views/forms/fields/slug-input.blade.php
+++ resources/views/forms/fields/slug-input.blade.php
@@ -97,20 +97,10 @@
 
                         <span>{{ $getVisitLinkLabel() }}</span>
 
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            class="h-4 w-4"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
+                        <x-heroicon-o-external-link
                             stroke-width="2"
-                        >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-                            />
-                        </svg>
+                            class="h-4 w-4"
+                        />
 
                     </a>
                 @endif
@@ -143,23 +133,13 @@
                     >
                         <span class="mr-1">&shy;{{ $getState() }}</span>
 
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
+                        <x-heroicon-o-pencil-alt
+                            stroke-width="2"
                             class="
                                 h-4 w-4
                                 text-primary-600 dark:text-primary-500
                             "
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            stroke-width="2"
-                        >
-                          <path
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
-                          />
-                        </svg>
+                        />
 
                         <span class="sr-only">{{ trans('filament-title-with-slug::package.permalink_action_edit') }}</span>
 
@@ -253,20 +233,10 @@
 
                                 <span>{{ $getVisitLinkLabel() }}</span>
 
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    class="h-4 w-4"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
+                                <x-heroicon-o-external-link
                                     stroke-width="2"
-                                >
-                                  <path
-                                      stroke-linecap="round"
-                                      stroke-linejoin="round"
-                                      d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-                                  />
-                                </svg>
+                                    class="h-4 w-4"
+                                />
 
                             </a>
 
